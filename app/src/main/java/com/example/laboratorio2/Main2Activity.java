447393c96d2d66ae5cb1e6d32e391324f0d29191
Sergio Muro <sergio.muroc@pucp.pe>
2020-05-18 08:21:53 -0500

    appbar y vistas

--- app/src/main/java/com/example/laboratorio2/Main2Activity.java
+++ app/src/main/java/com/example/laboratorio2/Main2Activity.java
@@ -1,21 +1,21 @@
 package com.example.laboratorio2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.appbar,menu);
+        getMenuInflater().inflate(R.menu.appbar2,menu);
         return true;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
+
 }
