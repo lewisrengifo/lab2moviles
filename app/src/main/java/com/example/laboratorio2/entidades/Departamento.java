3cd308ed11516330cdba31cfb0c0b485131101ea
Sergio Muro <sergio.muroc@pucp.pe>
2020-05-18 02:39:34 -0500

    entidades creadas

---
+++ app/src/main/java/com/example/laboratorio2/entidades/Departamento.java
@@ -0,0 +1,49 @@
+package com.example.laboratorio2.entidades;
+
+public class Departamento {
+    private int departmentId;
+    private String departmentName;
+    private String managerId;
+    private int locationId;
+    private String departmentShortName;
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getDepartmentShortName() {
+        return departmentShortName;
+    }
+
+    public void setDepartmentShortName(String departmentShortName) {
+        this.departmentShortName = departmentShortName;
+    }
+}
---
+++ app/src/main/java/com/example/laboratorio2/entidades/Empleado.java
@@ -0,0 +1,103 @@
+package com.example.laboratorio2.entidades;
+
+public class Empleado {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Trabajo[] jobId;
+    private Double salary;
+    private Double commissionPct;
+    private Jefe managerId;
+    private Departamento[] departmentId;
+    private String createdBy;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Trabajo[] getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Trabajo[] jobId) {
+        this.jobId = jobId;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getCommissionPct() {
+        return commissionPct;
+    }
+
+    public void setCommissionPct(Double commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public Jefe getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Jefe managerId) {
+        this.managerId = managerId;
+    }
+
+    public Departamento[] getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Departamento[] departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+}
